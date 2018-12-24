0cbe43759cec84bce1d25d4235fe071f1bc47786
juanfranciscofernandezherreros@bitbucket.org <juanfranciscofernandezherreros@bitbucket.org>
2018-12-24 13:13:54 +0100

    Routingç

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './usuarios/login/login.component';
 import { CountriesComponent } from './countries/countries.component';
-import {CountriesService} from './countries/countries.service';
+import { CountriesService } from './countries/countries.service';
+import { RouterModule , Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+
+const routes:Routes = [
+  {path:'',redirectTo:'/main', pathMatch:'full'},
+  {path:'countries',component:CountriesComponent},
+  {path:'main',component:MainComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    CountriesComponent
+    CountriesComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [CountriesService],
   bootstrap: [AppComponent]
