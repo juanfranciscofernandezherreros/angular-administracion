ee262544198d0e6035c9b70d4e7cfaf6aacbfa79
juanfranciscofernandezherreros@bitbucket.org <juanfranciscofernandezherreros@bitbucket.org>
2021-04-15 15:13:13 +0200

    feat : operation edit

--- src/app/_services/categories-edit.service.ts
+++ src/app/_services/categories-edit.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { Categories } from '../_models/categories';
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesEditService {
 
-  constructor() { }
+  public apiUrl: string;
+  private articles:string = environment.blogs;
+  
+  constructor(private http: HttpClient) { }
+
+  updateCategory(categories:Categories):Observable<any>{
+    return this.http.put(this.articles+"/api/protected/v1/category",categories);
+
+  }
 }
--- src/app/_services/categories-view.service.ts
+++ src/app/_services/categories-view.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesViewService {
 
-  constructor() { }
+  public apiUrl: string;
+  private articles:string = environment.blogs;
+  
+  constructor(private http: HttpClient) { }
+
+  getCategoryById(categoryId:number):Observable<any>{
+    return this.http.get(this.articles+"/api/public/v1/category?categoryId="+categoryId);
+
+  }
 }
